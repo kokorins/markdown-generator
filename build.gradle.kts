e15a35c8c98895a75af2ee3ac39c7c74076b1a7e
Sergey Kokorin <kokorins@yandex.ru>
2021-04-10 23:24:44 +0200

    Fixing the configuration for jitpack.io

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,13 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
+    maven // for jitpack
+    `maven-publish` // for jitpack
     kotlin("jvm") version "1.4.32"
     jacoco
 }
+
+group = "com.github.kokorins"
 
 repositories {
     mavenCentral()
